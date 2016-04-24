675f4910379c0d84abbf8d63d6d55d5418b82721
Dorian Grolaux <dgrolaux@nl.ibm.com>
2016-04-24 14:10:16 +0200

    bin/bash

--- mic_listen/StT.sh
+++ mic_listen/StT.sh
@@ -1,1 +1,5 @@
-echo 'StT.sh' && curl -u 8fb55af1-a8aa-4d50-8118-ab88248fcc0c:vNJfq3vTgI2K -X POST --header "Content-Type: audio/ogg;codecs=opus" --header "Transfer-Encoding: chunked" --data-binary $HOME/.node_app_slot/Weather.opus "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true" -o $HOME/.node_app_slot/output.json
+#!/bin/bash
+
+curl -u 8fb55af1-a8aa-4d50-8118-ab88248fcc0c:vNJfq3vTgI2K -X POST --header "Content-Type: audio/ogg;codecs=opus" --header "Transfer-Encoding: chunked" --data-binary Weather.opus "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true" -o output.json
+
+echo output.json
--- mic_listen/TtS.sh
+++ mic_listen/TtS.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl -u 4e9fc688-39d8-454c-b4ed-b7aa48157d4b:qQAWXaExzYsx -X POST --header "Content-Type: application/json" --header "Accept: audio/wav" --data-binary $HOME/.node_app_slot/output2.json "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize" -o TtS.wav
